bbc32b6f6c93062b96ddd349801a90638e9045aa
Hund <105028461+Jillberg@users.noreply.github.com>
2025-02-26 19:30:41 +0100

    bug fixed

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,7 +2,7 @@ import './index.scss'
 import AnimatedLetters from '../AnimatedLetters';
 import {useEffect,useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {  faCss3, faHtml5, faJsSquare, faReact, faUnity,faGitAlt} from '@fortawesome/free-brands-svg-icons';
+import {faReact, faUnity,faGitAlt} from '@fortawesome/free-brands-svg-icons';
 import { SiUnrealengine, SiBlender, SiGodotengine, } from 'react-icons/si';
 import Loader from 'react-loaders';
 
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,7 @@
 import './index.scss'
 import { Link , NavLink} from 'react-router-dom'
 import LogoS from '../../assets/images/logo-c.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome, faUser, faEnvelope, faSuitcase } from '@fortawesome/free-solid-svg-icons'
 import { faLinkedin, faYoutube, faGithub,  faItchIo } from '@fortawesome/free-brands-svg-icons'
@@ -38,23 +38,24 @@ const Sidebar = () => (
         </nav>
 
         <ul>
+
             <li>
-                <a target="_blank" rel="nonreferrer" href="https://www.linkedin.com/in/yichen-zhao-162b13340/">
+                <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/yichen-zhao-162b13340/">
                     <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e"/>
                 </a>
             </li>
             <li>
-                <a target="_blank" rel="nonreferrer" href="https://github.com/Jillberg?tab=repositories">
+                <a target="_blank" rel="noreferrer" href="https://github.com/Jillberg?tab=repositories">
                     <FontAwesomeIcon icon={faGithub} color="#4d4d4e"/>
                 </a>
             </li>
             <li>
-                <a target="_blank" rel="nonreferrer" href="https://www.youtube.com/@YichenZhao-y4w">
+                <a target="_blank" rel="noreferrer" href="https://www.youtube.com/@YichenZhao-y4w">
                     <FontAwesomeIcon icon={faYoutube} color="#4d4d4e"/>
                 </a>
             </li>
             <li>
-                <a target="_blank" rel="nonreferrer" href="https://itch.io/dashboard">
+                <a target="_blank" rel="noreferrer" href="https://itch.io/dashboard">
                     <FontAwesomeIcon icon={faItchIo} color="#4d4d4e"/>
                 </a>
             </li>
